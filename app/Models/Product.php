f78e1395aa0a48db083dc2500e00a2f4f8ef70ca
muathye <muath.ye@gmail.com>
2022-03-11 01:09:42 +0300

    Add fillable to Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description'
+    ];
 }
